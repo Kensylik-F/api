2d96fd6d35f5d25521697f187eb9ec0477a1d9f1
kensylik <152893677+Kensylik-F@users.noreply.github.com>
2024-12-22 13:26:12 +0300

    creating a logger for beautiful output to the console

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,37 @@
+import { Server } from 'http';
+import express, { Express } from 'express';
+import { useRouter } from './users/users';
+import { LoggerService } from './logger/logger.service';
 
 
-export class App {
+export class App{
+    app: Express;
+    port: number;
+    server: Server;
+    logger: LoggerService;
 
+
+    constructor(logger: LoggerService){
+        this.app = express(); 
+        this.port = 8000;
+        this.logger = logger;
+
+    }
+
+    useRoutes(){
+        this.app.use('/users', useRouter);
+    }
+
+    public async init(){
+        this.useRoutes();
+        this.server = this.app.listen(this.port);
+        this.logger.log(`Сервер запущен на http://localhost:${this.port}`)
+        // console.log(`Сервер запущен на http://localhost:${this.port}`);
+
+    }
 }
+
+
+
+
+
